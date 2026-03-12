9fcd3adb90e202f9cb32f9be9b2eace56fb33dc2
NicolasPedroso <nicolasgr.pedroso@gmail.com>
2026-03-12 09:58:11 -0300

    [FIX]Organização do projeto base melhorado

--- backend/springreact/src/main/java/br/ufpr/sislamir/SislamirApplication.java
+++ backend/springreact/src/main/java/br/ufpr/sislamir/SislamirApplication.java
@@ -1,4 +1,4 @@
-package com.example.springreact;
+package br.ufpr.sislamir;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/springreact/src/test/java/br/ufpr/sislamir/SpringreactApplicationTests.java
+++ backend/springreact/src/test/java/br/ufpr/sislamir/SpringreactApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.springreact;
+package br.ufpr.sislamir;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
